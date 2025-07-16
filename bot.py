712aa04a16179d9c43982e20b13a2ed523ad8d5b
Amir Sultanov <sultanovamir17@gmail.com>
2025-07-17 01:25:58 +0500

    Rename Procfile and add Flask webserver

--- bot.py
+++ bot.py
@@ -7,3 +7,13 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 app = ApplicationBuilder().token("8052314993:AAFRhqOhhEW6uGFuAdhNnNLM4pay-m9zXgE").build()
 app.add_handler(CommandHandler("start", start))
 app.run_polling()
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot is running"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
